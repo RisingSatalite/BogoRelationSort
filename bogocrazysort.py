5cdf827a1dcb4e311a8441458387e4bee969136f
RisingSatalite <107381820+RisingSatalite@users.noreply.github.com>
2024-10-05 16:23:32 -0600

    Update bogocrazysort.py

--- bogocrazysort.py
+++ bogocrazysort.py
@@ -2,93 +2,83 @@ import random
 from random import shuffle
 import copy
 
-#Use a parnet child relationship to rnadom assign nodes next to each other so that there is no need to examine them again
-
 class Node:
     def __init__(self, id):
-        self.id = id           # Unique identifier for the node
-        self.parents = []      # List of parent nodes
-        self.children = []     # List of child nodes
+        self.id = id  # Unique identifier for the node
+        self.parents = []  # List of parent nodes
+        self.children = []  # List of child nodes
 
-    # Method to add a child to this node
     def addChild(self, child):
-        self.children.append(child)  # Use append, not push
+        # Check if this creates a cycle
+        if self.creates_cycle(child):
+            print(f"Cannot link {self.id} to {child.id}, as it would create a cycle.")
+            return
+        self.children.append(child)
         child.parents.append(self)
 
-    # Method to remove this node
+    # Check if adding a child would create a cycle
+    def creates_cycle(self, child):
+        return child.isAncestorOf(self)
+
+    # Check if a node is an ancestor of this node
+    def isAncestorOf(self, node):
+        if self == node:
+            return True
+        return any(parent.isAncestorOf(node) for parent in self.parents)
+
     def remove(self):
-        # Remove this node from all its children's parents lists
         for child in self.children:
             child.parents = [parent for parent in child.parents if parent != self]
-
-        # Optionally: Set parents and children to empty arrays
         self.parents = []
         self.children = []
 
-    def removeChild(self, childID):
-        return False
-    
-    def parentChild(self, parentID):
-        return False
-
-    # Method to check if the node has no parents (is a root node)
     def isRoot(self):
-        return len(self.parents) == 0  # Use len() to get the length of the list
+        return len(self.parents) == 0
 
-    # Define a __repr__ method for better output
     def __repr__(self):
         return f"Node({self.id})"
 
-# Function to check if the list of nodes is sorted by their id
 def is_sorted(arr):
     for i in range(len(arr) - 1):
         if arr[i].id > arr[i + 1].id:
             return False
     return True
 
-def find_and_link(list, id1, id2):
-    nodesToLink = []
-    for items in list:
-        if(items.id == id2):
-            nodesToLink.append(items)
-    for items in list:
-        if(items.id == id1):
-            for i in nodesToLink:
-                items.addChild(i)
+def find_and_link(nodes, id1, id2):
+    parent_node = next((node for node in nodes if node.id == id1), None)
+    child_nodes = [node for node in nodes if node.id == id2]
+    for child in child_nodes:
+        parent_node.addChild(child)
 
 allNodes = []
 
-# Create an initial node and add it to the list
 a = Node(25)
 allNodes.append(a)
 
 for i in range(3):
-    a = Node(random.randint(1, 200))
-    allNodes.append(a)
+    allNodes.append(Node(random.randint(1, 200)))
 
 while True:
     new_list = copy.deepcopy(allNodes)
     shuffle(new_list)
     duplicate_list = []
 
-    #Check each node to see it is a root node and then adds it to duplicate_list
-    while(len(new_list) != 0):
-        for i in new_list:
-            if(i.isRoot() == 0):
+    while new_list:
+        for i in new_list[:]:
+            if i.isRoot():
                 duplicate_list.append(i)
                 new_list.remove(i)
-    
+
     if is_sorted(duplicate_list):
-        print(duplicate_list)  # This will now show [Node(25)]
+        print(duplicate_list)
         break
+
     print("Fail")
     print(duplicate_list)
-    previousItem = "initial"
+    
+    previousItem = None
     for i in duplicate_list:
-        if(previousItem == "initial"):
-            previousItem = i
-        else:
-            if(i.id > previousItem.id):
+        if previousItem:
+            if i.id > previousItem.id:
                 find_and_link(allNodes, i.id, previousItem.id)
-            previousItem = i
-    #break
+        previousItem = i
